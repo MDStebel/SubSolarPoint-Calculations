6c1cbdd0dd17290073d041730343b8247b4b1236
Michael Stebel <mdstebel@gmail.com>
2025-05-10 22:41:19 -0400

    Update SubSolarPoint.py

--- SubSolarPoint.py
+++ SubSolarPoint.py
@@ -1,156 +1,127 @@
 import math
-from datetime import datetime, timedelta, timezone
 import time
+from datetime import datetime, timezone, timedelta
 
 class Constants:
     coordinates_string_format = "{:3d}° {:02d}' {}  {:3d}° {:02d}' {}"
     degrees_longitude_per_hour = 15
-    degrees_to_radians = 0.01745329252
-    earth_tilt_in_degrees = 23.43715
+    degrees_to_radians = math.pi / 180
     earth_tilt_in_radians = 0.40905543478
-    julian_date_for_jan011970_at0000gmt = 2440587.5
+    julian_date_for_jan_01_1970 = 2440587.5
     noon_time = 12
     number_of_days_in_a_century = 36525
     number_of_days_in_a_year = 365
-    number_of_hours_in_a_day = 24
-    number_of_minutes_in_a_day = 1440
-    number_of_minutes_in_a_year = number_of_days_in_a_year * number_of_minutes_in_a_day
     number_of_minutes_in_an_hour = 60
+    number_of_minutes_in_a_day = 1440
     number_of_seconds_in_a_day = 86400
-    number_of_seconds_in_a_minute = 60
     number_of_seconds_in_a_year = number_of_seconds_in_a_day * number_of_days_in_a_year
     number_of_seconds_in_an_hour = 3600
-    one_eighty_degrees = 180
-    radians_to_degrees = 57.2957795131
+    radians_to_degrees = 180 / math.pi
     three_sixty_degrees = 360
+    one_eighty_degrees = 180
+    number_of_hours_in_a_day = 24
 
+def decimal_to_deg_min_sec(lat, lon):
+    def convert(coord, pos_dir, neg_dir):
+        total_seconds = int(coord * Constants.number_of_seconds_in_an_hour)
+        degrees = total_seconds // Constants.number_of_seconds_in_an_hour
+        seconds = abs(total_seconds % Constants.number_of_seconds_in_an_hour)
+        minutes = seconds // Constants.number_of_minutes_in_an_hour
+        direction = pos_dir if degrees >= 0 else neg_dir
+        return abs(degrees), minutes, direction
 
-class CoordinateConversions:
-    @staticmethod
-    def decimal_coordinates_to_deg_min_sec(latitude, longitude, format_str):
-        lat_seconds = int(latitude * Constants.number_of_seconds_in_an_hour)
-        lat_degrees = lat_seconds // int(Constants.number_of_seconds_in_an_hour)
-        lat_seconds = abs(lat_seconds % int(Constants.number_of_seconds_in_an_hour))
-        lat_minutes = lat_seconds // int(Constants.number_of_seconds_in_a_minute)
-        lat_seconds %= int(Constants.number_of_seconds_in_a_minute)
+    lat_d, lat_m, lat_dir = convert(lat, "North", "South")
+    lon_d, lon_m, lon_dir = convert(lon, "East", "West")
 
-        lon_seconds = int(longitude * Constants.number_of_seconds_in_an_hour)
-        lon_degrees = lon_seconds // int(Constants.number_of_seconds_in_an_hour)
-        lon_seconds = abs(lon_seconds % int(Constants.number_of_seconds_in_an_hour))
-        lon_minutes = lon_seconds // int(Constants.number_of_seconds_in_a_minute)
-        lon_seconds %= int(Constants.number_of_seconds_in_a_minute)
+    return Constants.coordinates_string_format.format(lat_d, lat_m, lat_dir, lon_d, lon_m, lon_dir)
 
-        lat_direction = "North" if lat_degrees >= 0 else "South"
-        lon_direction = "East" if lon_degrees >= 0 else "West"
+def julian_date_from_datetime(dt):
+    unix_time = dt.replace(tzinfo=timezone.utc).timestamp()
+    return Constants.julian_date_for_jan_01_1970 + unix_time / Constants.number_of_seconds_in_a_day
 
-        return format_str.format(abs(lat_degrees), lat_minutes, lat_direction,
-                                 abs(lon_degrees), lon_minutes, lon_direction)
+def julian_century_since_2000(dt):
+    return (julian_date_from_datetime(dt) - 2451545.0) / Constants.number_of_days_in_a_century
 
-    @staticmethod
-    def deg_min_sec_coordinates_to_decimal(degrees, minutes, seconds, direction):
-        sign = -1.0 if direction in ["S", "W"] else 1.0
-        return (degrees + (minutes + seconds / Constants.number_of_seconds_in_a_minute) /
-                Constants.number_of_seconds_in_a_minute) * sign
+def geometric_mean_longitude_sun(t):
+    return (280.46646 + t * 36000.76983 + t * t * 0.0003032) % Constants.three_sixty_degrees
 
+def mean_anomaly(t):
+    return 357.52911 + t * (35999.05029 - 0.0001537)
 
-class Astrocalculations:
-    @staticmethod
-    def jd_from_date(date):
-        jd = Constants.julian_date_for_jan011970_at0000gmt + date.timestamp() / Constants.number_of_seconds_in_a_day
-        return jd
+def eccentricity_of_earth_orbit(t):
+    return 0.016708634 - t * (0.000042037 + 0.0000001267 * t)
 
-    @staticmethod
-    def date_from_jd(jd):
-        seconds_since_epoch = (jd - Constants.julian_date_for_jan011970_at0000gmt) * Constants.number_of_seconds_in_a_day
-        date = datetime(1970, 1, 1, tzinfo=timezone.utc) + timedelta(seconds=seconds_since_epoch)
-        return date
+def sun_equation_of_center(t):
+    m = mean_anomaly(t)
+    m_rad = m * Constants.degrees_to_radians
+    return (math.sin(m_rad) * (1.914602 - t * (0.004817 + 0.000014 * t)) +
+            math.sin(2 * m_rad) * (0.019993 - 0.000101 * t) +
+            math.sin(3 * m_rad) * 0.000289)
 
-    @staticmethod
-    def julian_century_since_jan2000(date):
-        jc = (Astrocalculations.jd_from_date(date) - 2451545) / Constants.number_of_days_in_a_century
-        return jc
+def latitude_of_sun(dt):
+    t = julian_century_since_2000(dt)
+    geom_mean_long = geometric_mean_longitude_sun(t)
+    true_long = geom_mean_long + sun_equation_of_center(t)
+    true_long_rad = true_long * Constants.degrees_to_radians
+    lat_rad = math.asin(math.sin(true_long_rad) * math.sin(Constants.earth_tilt_in_radians))
+    return lat_rad * Constants.radians_to_degrees
 
-    @staticmethod
-    def orbit_eccentricity_of_earth(t):
-        ecc = 0.016708634 - t * (0.000042037 + 0.0000001267 * t)
-        return ecc
+def equation_of_time(dt):
+    t = julian_century_since_2000(dt)
+    L0 = geometric_mean_longitude_sun(t) * Constants.degrees_to_radians
+    e = eccentricity_of_earth_orbit(t)
+    M = mean_anomaly(t) * Constants.degrees_to_radians
+    obliquity = 0.0430264916545165  # ~23.44 degrees in radians
 
-    @staticmethod
-    def mean_anomaly(t):
-        m = 357.52911 + t * 35999.05029 - t * 0.0001537
-        return m
+    term1 = obliquity * math.sin(2 * L0)
+    term2 = 2 * e * math.sin(M)
+    term3 = 4 * e * obliquity * math.sin(M) * math.cos(2 * L0)
+    term4 = 0.5 * obliquity * obliquity * math.sin(4 * L0)
+    term5 = 1.25 * e * e * math.sin(2 * M)
 
-    @staticmethod
-    def equation_of_time(date):
-        t = Astrocalculations.julian_century_since_jan2000(date)
-        mean_longitude_sun_radians = Astrocalculations.geometric_mean_longitude_of_sun(t) * Constants.degrees_to_radians
-        mean_anomaly_sun_radians = Astrocalculations.mean_anomaly(t) * Constants.degrees_to_radians
-        eccentricity = Astrocalculations.orbit_eccentricity_of_earth(t)
-        obliquity = 0.0430264916545165
+    eot = 4 * (term1 - term2 + term3 - term4 - term5) * Constants.radians_to_degrees
+    return eot
 
-        term1 = obliquity * math.sin(2 * mean_longitude_sun_radians)
-        term2 = 2 * eccentricity * math.sin(mean_anomaly_sun_radians)
-        term3 = 4 * eccentricity * obliquity * math.sin(mean_anomaly_sun_radians) * math.cos(2 * mean_longitude_sun_radians)
-        term4 = 0.5 * obliquity * obliquity * math.sin(4 * mean_longitude_sun_radians)
-        term5 = 1.25 * eccentricity * eccentricity * math.sin(2 * mean_anomaly_sun_radians)
+def subsolar_longitude(dt):
+    eot = equation_of_time(dt)
+    local_time = dt.astimezone()
+    hour = local_time.hour + local_time.minute / 60.0
+    seconds_from_gmt = local_time.utcoffset().total_seconds()
 
-        equation_of_time = 4 * (term1 - term2 + term3 - term4 - term5) * Constants.radians_to_degrees
-        return equation_of_time
+    if seconds_from_gmt <= 0:
+        time_correction = 1
+        day_correction = 0
+    else:
+        time_correction = -1
+        day_correction = -Constants.number_of_hours_in_a_day
 
-    @staticmethod
-    def geometric_mean_longitude_of_sun(t):
-        sun_geom_mean_long = 280.46646 + t * 36000.76983 + t * t * 0.0003032
-        sun_geom_mean_long = sun_geom_mean_long % Constants.three_sixty_degrees
-        return sun_geom_mean_long
+    gmt_hour = (hour - seconds_from_gmt / Constants.number_of_seconds_in_an_hour -
+                time_correction * Constants.number_of_hours_in_a_day) % Constants.number_of_hours_in_a_day
 
-    @staticmethod
-    def latitude_of_sun(date):
-        t = Astrocalculations.julian_century_since_jan2000(date)
-        geom_mean_longitude = Astrocalculations.geometric_mean_longitude_of_sun(t)
-        sun_true_longitude = geom_mean_longitude + Astrocalculations.sun_equation_of_center(t)
-        sun_latitude = math.asin(math.sin(sun_true_longitude * Constants.degrees_to_radians) * math.sin(Constants.earth_tilt_in_radians))
-        sun_latitude_degrees = sun_latitude * Constants.radians_to_degrees
-        return sun_latitude_degrees
+    noon_delta = min(Constants.noon_time - gmt_hour - eot / Constants.number_of_minutes_in_an_hour + day_correction, 12.0)
+    subsolar_lon = noon_delta * Constants.degrees_longitude_per_hour
 
-    @staticmethod
-    def sun_equation_of_center(t):
-        m = Astrocalculations.mean_anomaly(t)
-        s_eoc = (math.sin(m * Constants.degrees_to_radians) * (1.914602 - t * (0.004817 + 0.000014 * t)) +
-                 math.sin(2 * m * Constants.degrees_to_radians) * (0.019993 - 0.000101 * t) +
-                 math.sin(3 * m * Constants.degrees_to_radians) * 0.000289)
+    if subsolar_lon < -Constants.one_eighty_degrees and gmt_hour <= Constants.noon_time:
+        lon_correction = Constants.one_eighty_degrees
+    elif subsolar_lon <= -Constants.one_eighty_degrees and gmt_hour >= Constants.noon_time:
+        lon_correction = 0
+    elif gmt_hour >= Constants.number_of_hours_in_a_day:
+        lon_correction = Constants.one_eighty_degrees
+    else:
+        lon_correction = 0
 
-        return s_eoc
+    return subsolar_lon + lon_correction
 
-    @staticmethod
-    def sub_solar_longitude_of_sun_at_current_time(date):
-        eot = Astrocalculations.equation_of_time(date)
-        local_time = date.hour + date.minute / Constants.number_of_minutes_in_an_hour
-        seconds_from_gmt = date.utcoffset().total_seconds() if date.utcoffset() else 0
+def get_subsolar_coordinates():
+    now = datetime.now()
+    lat = latitude_of_sun(now)
+    lon = subsolar_longitude(now)
+    return lat, lon
 
-        GMT = (local_time - seconds_from_gmt / Constants.number_of_seconds_in_an_hour) % Constants.number_of_hours_in_a_day
-        noon_delta = (Constants.noon_time - GMT - eot / Constants.number_of_minutes_in_an_hour) % Constants.number_of_hours_in_a_day
-        sub_solar_long = noon_delta * Constants.degrees_longitude_per_hour
-
-        if sub_solar_long < -Constants.one_eighty_degrees:
-            sub_solar_long += Constants.one_eighty_degrees
-
-        return sub_solar_long
-
-    @staticmethod
-    def get_sub_solar_coordinates():
-        now = datetime.now(timezone.utc)
-        lat = Astrocalculations.latitude_of_sun(now)
-        lon = Astrocalculations.sub_solar_longitude_of_sun_at_current_time(now)
-        print(lat,lon)
-        return lat, lon
-
-
-def main():
-    # secs_to_delay = 5
-    # while True:
-        subsolar_point = Astrocalculations.get_sub_solar_coordinates()
-
-
-
-if __name__ == "__main__":
-    main()
+# Main loop
+while True:
+    now = datetime.now()
+    lat, lon = get_subsolar_coordinates()
+    coords = decimal_to_deg_min_sec(lat, lon)
+    print(f"The subsolar point is now at: {coords} (at {now})")
+    time.sleep(5)
